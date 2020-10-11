9eb53976ea0e45ee6aa6ad5b3643c345037c5c80
Guillaume Caron <66323939+G-Caron@users.noreply.github.com>
2020-10-11 23:35:40 +0200

    Update exemple3.py
    Ajout des grids, de l'affichage, et amélioration des applications tests

--- exemple3.py
+++ exemple3.py
@@ -5,39 +5,72 @@ import numpy as np
 doc = cv2.imread(r'doc.png') 
 dog = cv2.imread(r'dog.png') 
 dogGray = cv2.imread(r'dog.png',cv2.IMREAD_GRAYSCALE)
-print(dogGray.shape)
-print(dogGray[0,0])
+# ~ print(dogGray.shape)
+# ~ print(dogGray[0,0])
   
-# COIN HAUT GAUCHE DU PATCH
-cornerTL = [30,100]
 
-# COIN BAS DROITE DU PATCH
-cornerBR = [150,350]
+def defineGrid(image, nbPatchH=None, nbPatchW=None, height=None, width=None):
+    
+    if ( (nbPatchH and not nbPatchW) or (not nbPatchH and nbPatchW) or
+        (height and not width) or (not height and width) or
+        ((nbPatchH or nbPatchW) and (height or width)) or 
+        not( nbPatchH or nbPatchW or height or width ) ):
+        raise ValueError("On attend (nbPatchH and nbPatchW) xor (h  and W)")
+
+    if (nbPatchH) :
+        height = image.shape[0]//nbPatchH
+        width = image.shape[1]//nbPatchW
+    else : 
+        nbPatchH = image.shape[0]//height
+        nbPatchW = image.shape[1]//width
+    print(height)
+    print(width)
+    print(nbPatchH)
+    print(nbPatchW)
+    
+    # Initialisation grid
+    grid = []
+    for i in range(nbPatchH):
+        grid.append([])
+        for j in range(nbPatchW):
+            grid[i].append({})
+
+    # Calcul des bornes du patch
+    for i in range(nbPatchH):
+        for j in range(nbPatchW):
+            grid[i][j]["top"] = i * height
+            grid[i][j]["left"] = j * width
+            grid[i][j]["bottom"] = (i+1) * height - 1
+            grid[i][j]["right"] = (j+1) * width - 1
+
+    return grid
 
 
-def insertPatch( image, cornerTL, cornerBR, color=[0,0,255]):
+# attention image est modifiée!!!
+def insertPatch( image, top, bottom, left, right, color=[0,0,255]):
     
     if len(image.shape) == 2:
-        image2 = np.repeat(image[:, :, np.newaxis], 3, axis=2)
-        print(image2.shape)  
-    else:
-        image2 = image.copy()
+        image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+        print(image.shape)  
+
     
     # Trait en haut
-    image2[cornerTL[0], cornerTL[1]:cornerBR[1]+1] = color
+    image[top, left:right+1] = color
 
     # Trait à droite
-    image2[cornerTL[0]:cornerBR[0]+1, cornerBR[1]] = color
+    image[top:bottom+1, right] = color
 
     # Trait en bas 
-    image2[cornerBR[0], cornerTL[1]:cornerBR[1]+1] = color
+    image[bottom, left:right+1] = color
 
     # Trait à gauche
-    image2[cornerTL[0]:cornerBR[0]+1, cornerTL[1]] = color
+    image[top:bottom+1, left] = color
     
-    return image2
+    return image
+
 
 def showImage(image, nomImage = ""):
+    
     # ~ # Window name in which image is displayed 
     window_name = nomImage
       
@@ -53,9 +86,59 @@ def showImage(image, nomImage = ""):
     cv2.destroyAllWindows()  
 
 
-doc2 = insertPatch(doc, cornerTL, cornerBR,[240,120,120])
-showImage(doc2)
-dog2 = insertPatch(dog, cornerTL, cornerBR)
-showImage(dog2)
-dog2Gray = insertPatch(dogGray, cornerTL, cornerBR)
-showImage(dog2Gray)
+def showGrid(image, grid):
+    
+    image2 = image.copy()
+    nph = len(grid)
+    npw = len(grid[0])
+    
+    for i in range(nph):
+        for j in range(npw):
+            insertPatch( image2, grid[i][j]["top"],grid[i][j]["bottom"],grid[i][j]["left"],grid[i][j]["right"])
+
+    showImage(image2)
+
+
+
+
+##################### APPLICATION 1 ###########################
+########## TEST DE LA FONCTION insertPATCH
+
+# Dimension du patch
+# ~ t = 30
+# ~ l = 100
+# ~ b = 150
+# ~ r = 350
+
+# ~ doc2 = insertPatch(doc, t,b,l,r,[240,120,120])
+# ~ showImage(doc2)
+# ~ dog2 = insertPatch(dog, t,b,l,r)
+# ~ showImage(dog2)
+# ~ dog2Gray = insertPatch(dogGray, t,b,l,r)
+# ~ showImage(dog2Gray)
+
+
+
+
+##################### APPLICATION 2 ###########################
+########## TEST DE LA FONCTION defineGrid
+
+# ~ nph, npw = 3, 5
+# ~ grid = defineGrid(doc, nbPatchH = nph, nbPatchW = npw)
+
+# ~ for i in range(nph):
+    # ~ for j in range(npw):
+        # ~ nom = "grid(" + str(i) + "," + str(j) + ") - top left corner : [" + str(grid[i][j]["top"]) + ", " + str(grid[i][j]["left"]) + "] - bottom right corner : [" + str(grid[i][j]["bottom"]) + ", " + str(grid[i][j]["right"]) + "]"
+        # ~ print(nom)
+        # ~ showImage( doc[grid[i][j]["top"] : grid[i][j]["bottom"]+1, grid[i][j]["left"]:grid[i][j]["right"]+1 ], nom)
+
+
+##################### APPLICATION 3 ###########################
+########## TEST DE LA FONCTION showGrid
+nph, npw = 8, 9
+h, w = 180, 220
+# ~ grid = defineGrid(doc, nbPatchH = nph, nbPatchW = npw)
+grid = defineGrid(doc, height = h, width = w)
+
+
+showGrid(doc,grid)
